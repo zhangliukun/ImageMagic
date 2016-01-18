32adbade913f05d2c96c994e64cfafab5e01ebae
zhangliukun <1433317488@qq.com>
2016-01-18 15:06:50 +0800

    add the camera prew

---
+++ ImageMagic/app/src/main/java/zalezone/imagemagic/opencv/cameraprew/CameraPrewActivity.java
@@ -0,0 +1,117 @@
+package zalezone.imagemagic.opencv.cameraprew;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.InstallCallbackInterface;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import zalezone.imagemagic.R;
+
+/**
+ * Created by zale on 16/1/14.
+ */
+public class CameraPrewActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+
+    public static void startActivity(Context context){
+        Intent intent = new Intent(context,CameraPrewActivity.class);
+        context.startActivity(intent);
+    }
+
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+
+    private Mat                    mRgba;
+    private Mat                    mGray;
+
+
+    static {
+        System.loadLibrary("opencv_java3");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_camera_prew);
+        initView();
+    }
+
+    private void initView() {
+        cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.fd_activity_surface_view);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+        return mRgba;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            super.onManagerConnected(status);
+            switch (status){
+                case LoaderCallbackInterface.SUCCESS:
+                    cameraBridgeViewBase.enableView();
+                    break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                }break;
+            }
+        }
+
+        @Override
+        public void onPackageInstall(int operation, InstallCallbackInterface callback) {
+            super.onPackageInstall(operation, callback);
+        }
+    };
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraBridgeViewBase.disableView();
+    }
+}
